dad02f168cf0f6928c40dec2e26d3eb4f5c7f983
MennaEraky <mennahesham62@yahoo.com>
2024-09-18 22:31:31 +0300

    Update 1-calculate.py

--- pages/1-calculate.py
+++ pages/1-calculate.py
@@ -1,6 +1,6 @@
 import streamlit as st
 st.header("session state ")
-if 'fruits' not in st.session_state
+if 'fruits' not in st.session_state:
   st.session_state.fruits=[]
 item = st.text_input("add item")
 btn=st.button("add")
