bd0c9b5875e4dd4ac553d26930317c11c6140169
MennaEraky <mennahesham62@yahoo.com>
2024-09-18 21:30:22 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,8 +43,13 @@ if file is not None:
       st.write(df[:num_row])
     num_col=df.select_dtypes(include='number').columns.to_list()
     
-    x_col=st.selectbox('choose x axis',num_col)
-    y_col=st.selectbox('choose y axis',num_col)
-    color=st.selectbox('choose color',num_col)
+    col1,col2,col3=st.columns(3)
+    with col1:
+      x_col=st.selectbox('choose x axis',num_col)
+    with col2:
+      y_col=st.selectbox('choose y axis',num_col)
+    with col3:
+      color=st.selectbox('choose color',num_col)
+
     fig=px.scatter(df,x=x_col,y=y_col,color=color)
     st.plotly_chart(fig)
