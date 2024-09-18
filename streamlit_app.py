5c9f140ebcf37ba0d9889950c5653acd513e8b9b
MennaEraky <mennahesham62@yahoo.com>
2024-09-18 21:23:16 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,17 +33,18 @@ import pandas as pd
 st.header("file upload app2")
 file = st.file_uploader("upload a file",type=['csv'])
 if file is not None:
-  df=pd.read_csv(file)
-  st.write(df)
-  num_row=st.slider('choose num rows',min_value=0,max_value=len(df),step=1)
-  names_col=st.multiselect('choose names of columns',df.columns.to_list())
-  if names_col:
+    df=pd.read_csv(file)
+    st.write(df)
+    num_row=st.slider('choose num rows',min_value=0,max_value=len(df),step=1)
+    names_col=st.multiselect('choose names of columns',df.columns.to_list())
+    if names_col:
       st.write(df[:num_row][names_col])
-  else:
+    else:
       st.write(df[:num_row])
-num_col=df.select_dtypes(include='number').columns.to_list()
-
-x_col=st.selectbox('choose x axis',num_col)
-y_col=st.selectbox('choose y axis',num_col)
-fig=px.scatter(df,x=x_col,y=y_col)
-st.plotly_chart(fig)
+    num_col=df.select_dtypes(include='number').columns.to_list()
+    
+    x_col=st.selectbox('choose x axis',num_col)
+    y_col=st.selectbox('choose y axis',num_col)
+    color=st.selectbox('choose color',num_col)
+    fig=px.scatter(df,x=x_col,y=y_col,color=color)
+    st.plotly_chart(fig)
