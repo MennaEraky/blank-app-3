5eddc7884e95570820cb48136f7da04feda6d954
MennaEraky <mennahesham62@yahoo.com>
2024-09-18 19:54:57 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit as st
 #app 1
 area=None
 st.header("calculate Area")
-choose=st.selectbos('choose the shape',['circle','rectangle'])
+choose=st.selectbox('choose the shape',['circle','rectangle'])
 if choose == 'circle':
     r=st.number_input("enter the radius",min_value=1,max_value=100)
     area=r*r*3.14
