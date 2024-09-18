acaad447f52e3995e10e0bfe01e55e103e5381ae
MennaEraky <mennahesham62@yahoo.com>
2024-09-18 20:13:41 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,16 +11,16 @@ st.header("calculate Area")
 
 with st.sidebar:
   choose=st.selectbox('choose the shape',['circle','rectangle'])
-    if choose == 'circle':
-        r=st.number_input("enter the radius",min_value=1,max_value=100)
-        area=r*r*3.14
-        st.write("area of circle is",area)
-    elif choose == 'rectangle':
-        w=st.number_input("enter the width",min_value=1,max_value=100)
-        h=st.number_input("enter the highet",min_value=1,max_value=100)
-        area=w*h
-        st.write("area of rectangle is",area)
+  if choose == 'circle':
+      r=st.number_input("enter the radius",min_value=1,max_value=100)
+      area=r*r*3.14
+      st.write("area of circle is",area)
+  elif choose == 'rectangle':
+      w=st.number_input("enter the width",min_value=1,max_value=100)
+      h=st.number_input("enter the highet",min_value=1,max_value=100)
+      area=w*h
+      st.write("area of rectangle is",area)
 
-    btn=st.button('calculate')
-    if btn:
-        st.write("the area is",area)
+  btn=st.button('calculate')
+  if btn:
+      st.write("the area is",area)
