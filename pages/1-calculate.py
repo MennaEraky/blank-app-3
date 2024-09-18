f0bd5cf5d6e8831804b4d26de1e14eafa2893439
MennaEraky <mennahesham62@yahoo.com>
2024-09-18 22:10:54 +0300

    Update 1-calculate.py

--- pages/1-calculate.py
+++ pages/1-calculate.py
@@ -1,1 +1,10 @@
 
+import streamlit as st
+import time
+import plotly.express as px 
+st.header("my first app ")
+st.sidebar.title("calculate Area app 1")
+name = st.text_input("enter your name")
+btn=st.button("show")
+if btn:
+    st.write("hello",name)
