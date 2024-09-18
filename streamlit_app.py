55d5c4ec1d7546de2041e533c7c3a460fecc3180
MennaEraky <mennahesham62@yahoo.com>
2024-09-18 19:54:22 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,19 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+#app 1
+area=None
+st.header("calculate Area")
+choose=st.selectbos('choose the shape',['circle','rectangle'])
+if choose == 'circle':
+    r=st.number_input("enter the radius",min_value=1,max_value=100)
+    area=r*r*3.14
+    st.write("area of circle is",area)
+elif choose == 'rectangle':
+    w=st.number_input("enter the width",min_value=1,max_value=100)
+    h=st.number_input("enter the highet",min_value=1,max_value=100)
+    area=w*h
+    st.write("area of rectangle is",area)
+
+btn=st.button('calculate')
+if btn:
+    st.write("the area is",area)
