80b6064652df6bbd982b2e9491e0ebe50acfa570
MennaEraky <mennahesham62@yahoo.com>
2024-09-18 21:15:41 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import time
-import ploty.streamlit as px
+import ploty.express as px
 st.header("my first app ")
 st.sidebar.title("calculate Area app 1")
 name = st.text_input("enter your name")
